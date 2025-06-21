7483642e32622a45bdff1815ec05cc2fab43411c
Keyner de Avila <kddeavila3@gmail.com>
2025-06-20 23:55:58 -0500

    Configure entity relationships and seed initial data in AppDbContext

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -5,6 +5,32 @@ namespace crud_api.Context;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    public DbSet<Employee> Employees {get; set; }
-    public DbSet<Profile> Profiles {get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Profile> Profiles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Profile>(table =>
+        {
+            table.HasKey(col => col.Id);
+            table.Property(col => col.Id).UseIdentityColumn().ValueGeneratedOnAdd();
+            table.Property(col => col.Name).HasMaxLength(50);
+            table.ToTable("Profile");
+            table.HasData(
+                new Profile { Id = 1, Name = "Programador Junior" },
+                new Profile { Id = 2, Name = "Programador Senior" },
+                new Profile { Id = 3, Name = "Analista de Sistemas" },
+                new Profile { Id = 4, Name = "Analista de Datos" }
+            );
+        });
+
+        modelBuilder.Entity<Employee>(table =>
+        {
+            table.HasKey(col => col.Id);
+            table.Property(col => col.Id).UseIdentityColumn().ValueGeneratedOnAdd();
+            table.Property(col => col.FullName).HasMaxLength(50);
+            table.HasOne(col => col.ProfileReference).WithMany(p => p.EmployeesReference).HasForeignKey(col => col.IdProfile);
+            table.ToTable("Employee");
+        });
+    }
 }
