a282bddfbbd41e6daec77406a18ca19029aff424
Keyner de Avila <kddeavila3@gmail.com>
2025-07-23 12:57:20 -0500

    Add comprehensive unit tests for ProfileService GetAll method
    - Add test cases: return all profiles, return empty list when no profiles exist, filtered profiles, and paginated profiles.
    - Update `.gitignore` to include common build artifacts, IDE-specific files, and OS-specific files.

--- crud-api.UnitTests/ProfileServiceTests.cs
+++ crud-api.UnitTests/ProfileServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using crud_api.Common;
 using crud_api.Context;
+using crud_api.DTOs.Profile;
 using crud_api.Entities;
 using crud_api.Services;
 using Microsoft.EntityFrameworkCore;
@@ -10,8 +11,142 @@ namespace crud_api.UnitTests;
 
 public class ProfileServiceTests
 {
-    private readonly DbContextOptions<AppDbContext> _dbContextOptions;
+    [Fact]
+    public async Task GetAll_ShouldReturnAllProfiles()
+    {
+        // Arrange
+        var dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "GetAll_ShouldReturnAllProfiles")
+            .Options;
 
+        await using var context = new AppDbContext(dbContextOptions);
+        
+        var profile1 = new Profile { Id = 1, Name = "Developer" };
+        var profile2 = new Profile { Id = 2, Name = "Analyst" };
+        await context.AddRangeAsync(profile1, profile2);
+        await context.SaveChangesAsync();
+
+        var profileQueryParams = new ProfileQueryParams();
+
+        var loggerMock = new Mock<ILogger<ProfileService>>();
+
+        var profileService = new ProfileService(context, loggerMock.Object);
+        
+        // Act
+        var result = await profileService.GetAllPaginatedAndFiltered(profileQueryParams);
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(ServiceResultStatus.Success, result.Status);
+        Assert.NotNull(result.Data);
+        Assert.Equal(2, result.Data!.TotalCount);
+    }
+
+    [Fact]
+    public async Task GetAll_ShouldReturnEmptyList_WhenNoProfilesExist()
+    {
+        // Arrange
+        var dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "GetAll_ShouldReturnEmptyList_WhenNoProfilesExist")
+            .Options;
+        
+        await using var context = new AppDbContext(dbContextOptions);
+        
+        var profileQueryParams = new ProfileQueryParams();
+
+        var loggerMock = new Mock<ILogger<ProfileService>>();
+        
+        var profileService = new ProfileService(context, loggerMock.Object);
+        
+        // Act
+        var result = await profileService.GetAllPaginatedAndFiltered(profileQueryParams);
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(ServiceResultStatus.Success, result.Status);
+        Assert.NotNull(result.Data);
+        Assert.Equal(0, result.Data!.TotalCount);
+    }
+
+    [Fact]
+    public async Task GetAll_ShouldReturnFilteredProfiles()
+    {
+        // Arrange
+        var dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "GetAll_ShouldReturnFilteredProfiles")
+            .Options;
+        
+        await using var context = new AppDbContext(dbContextOptions);
+        
+        var profile1 = new Profile { Id = 1, Name = "Developer" };
+        var profile2 = new Profile { Id = 2, Name = "Analyst" };
+        
+        await context.AddRangeAsync(profile1, profile2);
+        await context.SaveChangesAsync();
+
+        var profileQueryParams = new ProfileQueryParams()
+        {
+            Name = "Analyst"
+        };
+        
+        var loggerMock = new Mock<ILogger<ProfileService>>();
+        
+        var profileService = new ProfileService(context, loggerMock.Object);
+        
+        // Act
+        var result = await profileService.GetAllPaginatedAndFiltered(profileQueryParams);
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(ServiceResultStatus.Success, result.Status);
+        Assert.NotNull(result.Data);
+        Assert.Equal(1, result.Data!.TotalCount);
+        Assert.Equal(2, result.Data.Data![0].Id);
+        Assert.Equal("Analyst", result.Data!.Data[0].Name);
+    }
+
+    [Fact]
+    public async Task GetAll_ShouldReturnPaginatedProfiles()
+    {
+        // Arrange
+        var dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "GetAll_ShouldReturnPaginatedProfiles")
+            .Options;
+        
+        await using var context = new AppDbContext(dbContextOptions);
+        
+        var profile1 = new Profile { Id = 1, Name = "Developer" };
+        var profile2 = new Profile { Id = 2, Name = "Analyst" };
+        
+        await context.AddRangeAsync(profile1, profile2);
+        await context.SaveChangesAsync();
+
+        var profileQueryParams = new ProfileQueryParams()
+        {
+            QueryParams =
+            {
+                PageNumber = 2,
+                PageSize = 1
+            }
+        };
+        
+        var loggerMock = new Mock<ILogger<ProfileService>>();
+        
+        var profileService = new ProfileService(context, loggerMock.Object);
+        
+        // Act
+        var result = await profileService.GetAllPaginatedAndFiltered(profileQueryParams);
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(ServiceResultStatus.Success, result.Status);
+        Assert.NotNull(result.Data);
+        Assert.Equal(2, result.Data!.TotalCount);
+        Assert.Single(result.Data.Data!);
+        Assert.Equal(2, result.Data.Data![0].Id);
+        Assert.Equal("Analyst", result.Data!.Data[0].Name);
+    }
+    
     [Fact]
     public async Task GetById_ShouldReturnProfile_WhenProfileExists()
     {
