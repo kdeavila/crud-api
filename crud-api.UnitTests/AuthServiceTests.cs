a59f84d80173ada2d13661abdb79caf1ce58d098
Keyner de Avila <kddeavila3@gmail.com>
2025-07-26 23:36:55 -0500

    Add unit tests for AuthService
    - Implement test cases for `Login` with valid credentials.
    - Add tests for `Login` returning `NotFound` when the user does not exist.
    - Add tests for `Login` with invalid password.
    - Introduce `AuthServiceTests` class with shared `AppDbContext` and `AuthService` instances.
    - Utilize `IDisposable` pattern for test resource cleanup.

---
+++ crud-api.UnitTests/AuthServiceTests.cs
@@ -0,0 +1,130 @@
+using crud_api.Common;
+using crud_api.Context;
+using crud_api.DTOs.Auth;
+using crud_api.Entities;
+using crud_api.Services;
+using crud_api.Settings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace crud_api.UnitTests;
+
+public class AuthServiceTests : IDisposable
+{
+    private readonly AppDbContext _context;
+    private readonly AuthService _authService;
+    private readonly Mock<IOptions<JwtSettings>> _options;
+
+    public AuthServiceTests()
+    {
+        var dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        _context = new AppDbContext(dbContextOptions);
+        _options = new Mock<IOptions<JwtSettings>>();
+
+        var jwtSettings = new JwtSettings
+        {
+            SecretKey = "a_super_secret_key_that_is_long_enough_for_hs256",
+            Issuer = "test_issuer",
+            Audience = "test_audience",
+            ExpirationMinutes = 60
+        };
+
+        _options.Setup(o => o.Value).Returns(jwtSettings);
+        _authService = new AuthService(_context, _options.Object);
+    }
+
+    public void Dispose()
+    {
+        _context.Database.EnsureDeleted();
+        _context.Dispose();
+    }
+
+    [Fact]
+    public async Task Login_ShouldReturnJwt_WithValidCredentials()
+    {
+        // Arrange
+        const string password = "password_example";
+        var passwordHash = BCrypt.Net.BCrypt.HashPassword(password);
+
+        var user = new User()
+        {
+            Email = "email@example.com",
+            PasswordHash = passwordHash,
+            Role = UserRole.Admin
+        };
+
+        await _context.AddAsync(user);
+        await _context.SaveChangesAsync();
+
+        var userLoginDto = new UserLoginDto
+        {
+            Email = "email@example.com",
+            Password = password
+        };
+
+        // Act
+        var result = await _authService.Login(userLoginDto);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(ServiceResultStatus.Success, result.Status);
+        Assert.False(string.IsNullOrEmpty(result.JWT));
+        Assert.Equal("Login successful", result.Message);
+    }
+
+    [Fact]
+    public async Task Login_ShouldReturnNotFound_WhenUserDoesNotExist()
+    {
+        var userLoginDto = new UserLoginDto
+        {
+            Email = "email@example.com",
+            Password = "password_example"
+        };
+        
+        // Act
+        var result = await _authService.Login(userLoginDto);
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(ServiceResultStatus.NotFound, result.Status);
+        Assert.Equal("User not found", result.Message);
+        Assert.Null(result.JWT);
+    }
+
+    [Fact]
+    public async Task Login_ShouldReturnInvalidInput_WhenThePasswordIsInvalid()
+    {
+        // Arrange
+        const string password = "password_example";
+        var passwordHash = BCrypt.Net.BCrypt.HashPassword(password);
+
+        var user = new User()
+        {
+            Email = "email@example.com",
+            PasswordHash = passwordHash,
+            Role = UserRole.Admin
+        };
+
+        await _context.AddAsync(user);
+        await _context.SaveChangesAsync();
+
+        var userLoginDto = new UserLoginDto
+        {
+            Email = "email@example.com",
+            Password = "password_invalid"
+        };
+        
+        // Act
+        var result = await _authService.Login(userLoginDto);
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(ServiceResultStatus.InvalidInput, result.Status);
+        Assert.Equal("Invalid password", result.Message);
+        Assert.Null(result.JWT);
+    }
+}
